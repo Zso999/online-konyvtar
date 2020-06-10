88683f2dec8037f2d813009488ff1a6b738d3685
Zso999 <techtabor@enyim.hu>
2020-06-10 21:45:03 +0200

    line

--- public/blog/blog.js
+++ public/blog/blog.js
@@ -286,7 +286,7 @@ function kiirHozzaszolas(hozzaszolasok, bejegyzes){
   em.setAttribute("class", "hfejlec");
 
   var dszov = document.createElement("DIV");
-  var dsz = document.createTextNode(hozzaszolasok.uzenet);
+  var dsz = document.createTextNode(hozzaszolasok.uzenet + <hr>);
   dszov.appendChild(dsz);
   dki.appendChild(dszov);
 
